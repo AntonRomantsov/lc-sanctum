9619270df58a8667cb2580cc4906d970d5e28083
Anton Romantsov <aromantsov1@gmail.com>
2025-08-22 00:36:24 +0300

    postman

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['middleware' => 'auth:sanctum'], function(){
+   Route::get('/get', 'GetController@index'); 
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
